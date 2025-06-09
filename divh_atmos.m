8ad8e9afbc2ac75d4375d0ffb94371e6d2694d15
Alphann <150248754+Naimba@users.noreply.github.com>
2025-06-09 21:33:25 +0800

    Delete divh_atmos.m

--- divh_atmos.m
+++
@@ -1,9 +0,0 @@
-function divh=divh_atmos(longitude, latitude, u, v)
-    R=6378137.0;             % 赤道半径
-    dx=dx_atmos(longitude, latitude);
-    dy=dy_atmos(latitude);
-    [du, ~]=gradient(u);
-    [~, dv]=gradient(v);
-    divh=du./dx+dv./dy-v.*tan(latitude.*pi./180)./R;  % 减号后面为 纬圈周长随纬度变化的相对变化率
-    divh(abs(latitude)==90)=NaN;
-end
